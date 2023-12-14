91cc44029cadc30b1db814fbf82a76deef746aaa
Dario <dramirez@balanzashook.com.ar>
2023-12-14 15:42:07 -0300

    Animacion en cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 export const App = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+  
       <div className="container-cards">
         <Card imagen="../src/assets/chips.png" />
         <Card imagen="../src/assets/diyuntor.png"/>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,43 @@
 
 
 
-import React from 'react'
+import React, { useState } from 'react'
+import Pulse from 'react-reveal/Pulse';
 
 import '../styles.css'
 
 export const Card = ( {imagen} ) => {
+
+  const [imageState, setImageState] = useState(false);
+
+  const onFocusImage = () => {
+   
+    setImageState(true);
+  }
+
+  const onMouseOut = () =>{
+    setImageState(false);
+  }
+
   return (
+
     <div className='card'>
-      <img className='card-img' src={ imagen } alt="" />
+      {
+        imageState
+       
+        ?<Pulse>
+            <img 
+              onMouseEnter={ onFocusImage }
+              onMouseLeave={ onMouseOut }
+              className='card-img' src={ imagen } alt="" />
+          </Pulse>
+        :
+        <img 
+              onMouseEnter={ onFocusImage }
+              onMouseLeave={ onMouseOut }
+              className='card-img' src={ imagen } alt="" />
+      }
+      
     </div>
   )
 }
